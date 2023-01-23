2890841aea808952ff20557e57dcaa629aaea473
MORENCY1990 <110705429+MORENCY1990@users.noreply.github.com>
2023-01-23 17:44:21 -0300

    Add files via upload

---
+++ CURSODB/Teste_Logica/src/testMain/Exemplo_Logica.java
@@ -0,0 +1,11 @@
+package testMain;
+
+public class Exemplo_Logica {
+    public static void main(String[] args) {
+        int area = 2*2;
+        double soma = 3.9 + 8.4;
+        soma += area;
+        System.out.println(soma);
+        
+    }
+}
