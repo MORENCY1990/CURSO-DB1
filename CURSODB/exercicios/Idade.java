5324fbf6989fe0d77024d4640abea8e36892ef83
MORENCY1990 <110705429+MORENCY1990@users.noreply.github.com>
2023-01-25 01:53:05 -0300

    Exercícos 1, 2 e 3 realizados no VS CODE

---
+++ CURSODB/exercicios/Idade.java
@@ -0,0 +1,11 @@
+public class Idade {
+	public static void main(String[] args) {
+		int idade = 17;
+
+		if (idade >= 18) {
+			System.out.println("parabens, você é maior");
+		} else {
+			System.out.println("tu é menor");
+		}
+	}
+}
---
+++ CURSODB/exercicios/Venda.java
@@ -0,0 +1,28 @@
+public class Venda {
+
+    public static void main(String[] args) {
+        String nome = "Caneta";
+        double preco = 2;
+        double quant = 11;
+
+        if(quant >50){
+            double F = (preco * quant);
+            double desc = F *0.25;
+            double precoF = F - desc;
+            System.out.println("Item comprado: "+ nome+ ". Valor Total: R$"+precoF);
+        }else if(quant>=21 && quant <=50){
+            double F = (preco * quant);
+            double desc = F *0.20;
+            double precoF = F - desc;
+            System.out.println("Item comprado: "+ nome+ ". Valor Total: R$"+precoF);
+        } else if(quant >=11 && quant <=20){
+            double F = (preco * quant);
+            double desc = F *0.10;
+            double precoF = F - desc;
+            System.out.println("Item comprado: "+ nome+ ". Valor Total: R$"+precoF);
+        } else{
+            double F = (preco * quant);
+            System.out.println("Item comprado: "+ nome+ ". Valor Total: R$"+F);
+        }
+    }
+}
