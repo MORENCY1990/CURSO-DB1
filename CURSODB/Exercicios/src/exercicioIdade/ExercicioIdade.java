4bb1305c8e374a916f2f294bb5f682ffc74fdd12
MORENCY1990 <110705429+MORENCY1990@users.noreply.github.com>
2023-01-25 11:57:08 -0300

    Commit para add exercícios feitos no eclipse

---
+++ CURSODB/Exercicios/src/exercicioIdade/ExercicioIdade.java
@@ -0,0 +1,14 @@
+package exercicioIdade;
+
+public class ExercicioIdade {
+    public static void main(String[] args) {
+        int idade = 17;
+
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+
+}
