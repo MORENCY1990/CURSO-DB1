255429d2e90dddd1171500ff1acd1d9b661657f9
MORENCY1990 <110705429+MORENCY1990@users.noreply.github.com>
2023-01-29 01:29:57 -0300

    Add exercícios 13 e 15

---
+++ CURSODB/ExerciciosCasa/src/exercicio15/Numeros.java
@@ -0,0 +1,22 @@
+package exercicio15;
+
+public class Numeros {
+	
+	private float numero;
+	private float resul;
+	
+	public Numeros () {
+		
+		this.numero = numero;
+		
+	}
+	public void setNumero(float numero) {
+		this.numero = numero;
+	}
+	public float getNumero() {
+		return this.numero;
+	}
+	
+	
+	
+}
